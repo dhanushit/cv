962fd9866c2ac9d3e4e2a073961ec5333b007c88
dhanushit <124713579+dhanushit@users.noreply.github.com>
2023-04-08 23:00:33 +0530

    udated animation

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-// Can also be included with a regular script tag
-// import Typed from 'typed.js';
-
-// const observer = new IntersectionObserver((entries) => {
-//     entries.forEach((entry)=>{
-//         console.log(entry);
-//         if(entry.isIntersecting){
-//             entry.target.classList.add('show');
-//         }else{
-//             entry.target.classList.remove('show');
-//         }
-//     })
-// })
-
-// const hiddenElement = document.querySelectorAll('.animation-section');
-
-// hiddenElement.forEach((el)=> observer.observe(el));
-
-
 
 var typed = new Typed('.text-animation', {
     strings: ["Web Developer", "Mobile Application Developer"],
