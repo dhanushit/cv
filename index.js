dabf8d1d563770960e0ce812d060e69e4c7c3668
dhanushit <124713579+dhanushit@users.noreply.github.com>
2023-07-09 22:25:18 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
 
 var typed = new Typed('.text-animation', {
-    strings: ["Web Developer", "Mobile Application Developer"],
+    strings: ["Web Developer", "App Developer"],
     typeSpeed: 40,
     backSpeed: 40,
     backDelay: 1000,
     loop: true
   });
+
+var curs = document.getElementById("cursor");
+
+document.addEventListener("mousemove", function (event) {
+    curs.style.left = event.x + 20 + "px";
+    curs.style.top = event.y + 20 + "px";
+
+});
+
+var bottomnav = document.querySelector(".bottom-nav");
+var menu = document.querySelector(".menu");
+var close = document.querySelector(".close");
+var bottomnavcontent = document.querySelector(".bottom-nav-content-box")
+
+
+
+function slide() {
+   
+
+    if(bottomnav.classList.contains('visible')){
+        bottomnavcontent.classList.toggle('bottom-nav-visible');
+        menu.classList.toggle("menuinvisible");
+        close.classList.toggle("menuinvisible");
+        setTimeout(()=>{
+           bottomnav.classList.toggle('visible');
+        },1000);
+      return;
+    }
+
+
+    bottomnav.classList.toggle("visible");
+    menu.classList.toggle("menuinvisible");
+    close.classList.toggle("menuinvisible");
+    bottomnavcontent.classList.toggle('bottom-nav-visible');
+   
+}
+
+
+
+
